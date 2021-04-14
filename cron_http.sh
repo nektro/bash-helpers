a2e2e9887b4e15d9937fdc77a234867a1384f911
Meghan Denny <hello@nektro.net>
2021-04-13 23:54:58 -0700

    add cron_http.sh

---
+++ cron_http.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Usage: ./cron_http.sh <url> <wait in hrs>
+# Calls ./generate.sh after finding new updates to <url>
+
+most_recent=''
+
+while true
+do
+    headers=$(curl -s -X HEAD -v $1 2>&1)
+    etag=$(echo "$headers" | grep 'etag')
+
+    if [ -z "$most_recent" ]
+    then
+        most_recent="$etag"
+        echo "first run of loop, $etag"
+        continue
+    fi
+
+    if [ "$most_recent" == "$etag" ]
+    then
+        echo "most recent etag is the same, sleeping"
+        # sec - min - hr
+        sleep $((1 * 60 * 60 * $2))
+        continue
+    fi
+
+    echo "found new $etag"
+    ./generate.sh
+done
